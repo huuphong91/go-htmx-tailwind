92969d2e2586b73487312e9682f8df745c1b58f0
Huu Phong <huuphong91@gmail.com>
2024-12-05 07:23:05 +0000

    Add new HTML templates for user authentication and profile management; create user model and update dependencies

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"go-htmx-tailwind-app/internal/handlers"
 	"html/template"
 	"log"
 	"mime"
 	"net/http"
-	"strconv"
-	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 )
 
 var tmpl *template.Template
 var db *sql.DB
+
+var Store = sessions.NewCookieStore([]byte("usermanagementsecret"))
 
 type Task struct {
 	Id   int
@@ -25,12 +26,19 @@ type Task struct {
 
 func init() {
 	tmpl, _ = template.ParseGlob("web/templates/*.html")
+
+		//Set up Sessions
+	Store.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   3600 * 3,
+		HttpOnly: true,
+	}
 }
 
 func initDB() {
 	var err error
 	// Initialize the db variable
-	db, err = sql.Open("mysql", "root:CxjTikmIOw@(mysql.default.svc.cluster.local:3306)/my_database?parseTime=true")
+	db, err = sql.Open("mysql", "root:CxjTikmIOw@(mysql.default.svc.cluster.local:3306)/usermanagement?parseTime=true")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -42,282 +50,41 @@ func initDB() {
 }
 
 func main() {
-    // Set MIME type for CSS files
-    mime.AddExtensionType(".css", "text/css")
+  // Set MIME type for CSS files
+  mime.AddExtensionType(".css", "text/css")
 
-    gRouter := mux.NewRouter()
+  gRouter := mux.NewRouter()
 
 	//Setup MySQL
 	initDB()
 	defer db.Close()
 
-	gRouter.HandleFunc("/", Homepage)
-    // Serve static files
-    fs := http.FileServer(http.Dir("web/static"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
+  // Serve static files
+  fs := http.FileServer(http.Dir("web/static"))
+  http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    // Serve the home page
-    // http.HandleFunc("/", HomeHandler)
+	fileServer := http.FileServer(http.Dir("uploads"))
+	gRouter.PathPrefix("/uploads/").Handler(http.StripPrefix("/uploads", fileServer))
 
-    // if err := http.ListenAndServe(":8000", nil); err != nil {
-    //     log.Fatal(err)
-    // }
+	gRouter.HandleFunc("/", handlers.Homepage(db, tmpl, Store))
 
-    //Get Tasks
-	gRouter.HandleFunc("/tasks", fetchTasks).Methods("GET")
+	gRouter.HandleFunc("/register", handlers.RegisterPage(db, tmpl)).Methods("GET")
 
-	//Fetch Add Task Form
-	gRouter.HandleFunc("/newtaskform", getTaskForm)
+	gRouter.HandleFunc("/register", handlers.RegisterHandler(db, tmpl)).Methods("POST")
 
-	//Add Task
-	gRouter.HandleFunc("/tasks", addTask).Methods("POST")
+	gRouter.HandleFunc("/login", handlers.LoginPage(db, tmpl)).Methods("GET")
 
-	//Fetch Update Form
-	gRouter.HandleFunc("/gettaskupdateform/{id}", getTaskUpdateForm).Methods("GET")
+	gRouter.HandleFunc("/login", handlers.LoginHandler(db, tmpl, Store)).Methods("POST")
 
-	//Update Task
-	gRouter.HandleFunc("/tasks/{id}", updateTask).Methods("PUT", "POST")
+	gRouter.HandleFunc("/edit", handlers.Editpage(db, tmpl, Store)).Methods("GET")
 
-	//Delete Task
-	gRouter.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
+	gRouter.HandleFunc("/edit", handlers.UpdateProfileHandler(db, tmpl, Store)).Methods("POST")
+
+	gRouter.HandleFunc("/upload-avatar", handlers.AvatarPage(db, tmpl, Store)).Methods("GET")
+
+	gRouter.HandleFunc("/upload-avatar", handlers.UploadAvatarHandler(db, tmpl, Store)).Methods("POST")
+
+	gRouter.HandleFunc("/logout", handlers.LogoutHandler(Store)).Methods("GET")
 
 	http.ListenAndServe(":8000", gRouter)
-}
-
-func Homepage(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTemplate(w, "index.html", nil)
-}
-
-// func HomeHandler(w http.ResponseWriter, r *http.Request) {
-//     tmpl, err := template.ParseFiles("web/templates/index.html")
-//     if err != nil {
-//         http.Error(w, err.Error(), http.StatusInternalServerError)
-//         return
-//     }
-//     tmpl.Execute(w, nil)
-// }
-
-
-func fetchTasks(w http.ResponseWriter, r *http.Request) {
-	todos, _ := getTasks(db)
-	//fmt.Println(todos)
-
-	//If you used "define" to define the template, use the name you gave it here, not the filename
-	tmpl.ExecuteTemplate(w, "todoList", todos)
-}
-
-func getTaskForm(w http.ResponseWriter, r *http.Request) {
-
-	tmpl.ExecuteTemplate(w, "addTaskForm", nil)
-}
-
-func addTask(w http.ResponseWriter, r *http.Request) {
-
-	task := r.FormValue("task")
-
-	fmt.Println(task)
-
-	query := "INSERT INTO tasks (task, done) VALUES (?, ?)"
-
-	stmt, err := db.Prepare(query)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	_, executeErr := stmt.Exec(task, 0)
-
-	if executeErr != nil {
-		log.Fatal(executeErr)
-	}
-
-	// Return a new list of Todos
-	todos, _ := getTasks(db)
-
-	//You can also just send back the single task and append it
-	//I like returning the whole list just to get everything fresh, but this might not be the best strategy
-	tmpl.ExecuteTemplate(w, "todoList", todos)
-
-}
-
-func getTaskUpdateForm(w http.ResponseWriter, r *http.Request) {
-
-	vars := mux.Vars(r)
-
-	//Convert string id from URL to integer
-	taskId, _ := strconv.Atoi(vars["id"])
-
-	task, err := getTaskByID(db, taskId)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
-	tmpl.ExecuteTemplate(w, "updateTaskForm", task)
-
-}
-
-func updateTask(w http.ResponseWriter, r *http.Request) {
-
-	vars := mux.Vars(r)
-
-	taskItem := r.FormValue("task")
-	//taskStatus, _ := strconv.ParseBool(r.FormValue("done"))
-	var taskStatus bool
-
-	fmt.Println(r.FormValue("done"))
-
-	//Check the string value of the checkbox
-	switch strings.ToLower(r.FormValue("done")) {
-	case "yes", "on":
-		taskStatus = true
-	case "no", "off":
-		taskStatus = false
-	default:
-		taskStatus = false
-	}
-
-	taskId, _ := strconv.Atoi(vars["id"])
-
-	task := Task{
-		taskId, taskItem, taskStatus,
-	}
-
-	updateErr := updateTaskById(db, task)
-
-	if updateErr != nil {
-		log.Fatal(updateErr)
-	}
-
-	//Refresh all Tasks
-	todos, _ := getTasks(db)
-
-	tmpl.ExecuteTemplate(w, "todoList", todos)
-}
-
-func deleteTask(w http.ResponseWriter, r *http.Request) {
-
-	vars := mux.Vars(r)
-
-	taskId, _ := strconv.Atoi(vars["id"])
-
-	err := deleTaskWithID(db, taskId)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
-	//Return list
-	todos, _ := getTasks(db)
-
-	tmpl.ExecuteTemplate(w, "todoList", todos)
-}
-
-func getTasks(dbPointer *sql.DB) ([]Task, error) {
-
-	query := "SELECT id, task, done FROM tasks"
-
-	rows, err := dbPointer.Query(query)
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-	var tasks []Task
-
-	for rows.Next() {
-		var todo Task
-
-		rowErr := rows.Scan(&todo.Id, &todo.Task, &todo.Done)
-
-		if rowErr != nil {
-			return nil, err
-		}
-
-		tasks = append(tasks, todo)
-	}
-
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return tasks, nil
-
-}
-
-func getTaskByID(dbPointer *sql.DB, id int) (*Task, error) {
-
-	query := "SELECT id, task, done FROM tasks WHERE id = ?"
-
-	var task Task
-
-	row := dbPointer.QueryRow(query, id)
-	err := row.Scan(&task.Id, &task.Task, &task.Done)
-
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return nil, fmt.Errorf("No task was found with task %d", id)
-		}
-		return nil, err
-	}
-
-	return &task, nil
-
-}
-
-func updateTaskById(dbPointer *sql.DB, task Task) error {
-
-	query := "UPDATE tasks SET task = ?, done = ? WHERE id = ?"
-
-	result, err := dbPointer.Exec(query, task.Task, task.Done, task.Id)
-
-	if err != nil {
-		return err
-	}
-
-	rowsAffected, err := result.RowsAffected()
-
-	if err != nil {
-		return err
-	}
-
-	if rowsAffected == 0 {
-		fmt.Println("No rows updated")
-	} else {
-		fmt.Printf("%d row(s) updated\n", rowsAffected)
-	}
-
-	return nil
-
-}
-
-func deleTaskWithID(dbPointer *sql.DB, id int) error {
-
-	query := "DELETE FROM tasks WHERE id = ?"
-
-	stmt, err := dbPointer.Prepare(query)
-
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	result, err := stmt.Exec(id)
-	if err != nil {
-		return err
-	}
-
-	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-		return err
-	}
-	if rowsAffected == 0 {
-		return fmt.Errorf("no task found with id %d", id)
-	}
-
-	fmt.Printf("Deleted %d task(s)\n", rowsAffected)
-	return nil
-
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,14 @@ module go-htmx-tailwind-app
 go 1.23.3
 
 require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/sessions v1.4.0
+	golang.org/x/crypto v0.29.0
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/go-sql-driver/mysql v1.8.1 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
 )
